e0e5e12bc5b85738888808838026d806f2b2d231
gregory kelleher <gregory@gregorykelleher.com>
2017-10-23 20:51:21 +0100

    cleared cache and updated gitignore

--- cache/compiled/files/4b3ba1edf06cb67ed95d5bc48d370be0.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/admin/Documents/sites/theoneyearproject.ie/user/config/streams.yaml',
-    'modified' => 1475680529,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/bd431152c9239669db818b0ccfcf0269.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://soraarticle/soraarticle.yaml',
-    'modified' => 1475680532,
-    'data' => [
-        'enabled' => true
-    ]
-];
